b8ec71f3fbc6b7e41f7fac6da9954992912da89c
Mehedi Hassan <mehedihassan2046@gmail.com>
2023-11-05 11:48:55 +0600

    add javascript code for dynamic the function

---
+++ Toggle.js
@@ -0,0 +1,7 @@
+const toggle = document.querySelector(".toggle");
+const toggleDiv =document.querySelector(".toggleDiv")
+function toggleBtn(){
+    toggle.classList.toggle("active");
+    toggleDiv.classList.toggle("active");
+}
+
